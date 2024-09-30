d7c4ab20e129c98b8cfb249682ba488010facc03
yeonsu <yeonsu9205@naver.com>
2024-09-30 20:58:28 +0900

    가만히있을때 세팅

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -2,12 +2,12 @@ from pico2d import *
 
 open_canvas()
 ground = load_image('TUK_GROUND.png')
-standmiku = load_image('standmiku1.png')
+character = load_image('standmiku1.png') #가만히 서있는거
 
 def handle_events():
     global running
 
-    global dir
+    global dirRL
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -15,31 +15,55 @@ def handle_events():
         # fill here
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                dir += 1
+                dirRL += 1
             elif event.key == SDLK_LEFT:
-                dir -= 1
+                dirRL -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                dir -=1
+                dirRL -=1
             if event.key ==SDLK_LEFT:
-                dir +=1
+                dirRL +=1
 
 
 running = True
 x = 800 // 2
 frame = 0
-dir =0
-# fill here
+dirRL =0
+
+standFrame = 20
+runFrame = 8
+
+def standCha(): #가만히 서있을 때 함수
+    global frame
+
+    print("stand")
+    character = load_image('standmiku1.png')  # 가만히 서있는거
+    character.clip_draw(frame * 59, 0, 59, 67, x, 90)
+    frame = (frame + 1) % standFrame
+
 while running:
     clear_canvas()
-    ground.draw(400,0)
-    standmiku.clip_draw(frame*77,0,77,67,x,90)
+    ground.draw(400,300,800,600)
+
+    standCha()
+    # 가만히 서있을 때
+    # ~~
+
+    # 달릴 때
+    # ~~
+
+    # 아래로 갈 때
+    # ~~
+
+    # 위로 갈 때
+    # ~~
+
     update_canvas()
     handle_events()
-    frame = (frame+1)%8
-    x += dir*15
+
+    x += dirRL*15
     delay(0.05)
 
 close_canvas()
